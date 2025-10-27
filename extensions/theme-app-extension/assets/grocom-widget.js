a5ba19455fe325f8e49dc7774143ae7b70f94999
ainguyen <hoaiaic7@gmail.com>
2025-10-27 17:25:48 +0700

    updaet code conflict

--- extensions/theme-app-extension/assets/grocom-widget.js
+++ extensions/theme-app-extension/assets/grocom-widget.js
@@ -1,3 +1,55 @@
+ document.addEventListener('DOMContentLoaded', async () => {
+    const el = document.getElementById('review-section');
+    if (!el) return;
+
+    const productId = el.dataset.productId;
+    const shop = Shopify.shop;
+    const linkApp = 'https://be-gearo.vinetawp.com/api';
+
+    try {
+      const res = await fetch(`${linkApp}/review-box?product_id=${productId}&shop=${shop}`);
+      const data = await res.json();
+      el.innerHTML = data.html;
+    
+      //     // ------------------------------
+    let handle = null;
+    let title = null;
+
+    // Get handle
+    if (window.ShopifyAnalytics?.meta?.product?.handle) {
+      handle = window.ShopifyAnalytics.meta.product.handle;
+    } else if (window.meta?.product?.handle) {
+      handle = window.meta.product.handle;
+    } else {
+      const parts = window.location.pathname.split('/');
+      const index = parts.indexOf('products');
+      if (index !== -1 && parts[index + 1]) handle = parts[index + 1];
+    }
+
+    // Get title
+    if (window.ShopifyAnalytics?.meta?.product?.title) {
+      title = window.ShopifyAnalytics.meta.product.title;
+    } else if (window.meta?.product?.title) {
+      title = window.meta.product.title;
+    } else {
+      const elTitle = document.querySelector('h1.product__title, .product-title, [data-product-title]');
+      if (elTitle) title = elTitle.textContent.trim();
+    }
+
+    // Assign values to form inputs
+    const inputHandle = el.querySelector('input[name="handle"]');
+    if (inputHandle && handle) inputHandle.value = handle;
+    else console.warn('Input[name="handle"] not found or handle not detected');
+
+    const inputTitle = el.querySelector('input[name="product_title"]');
+    if (inputTitle && title) inputTitle.value = title;
+    else console.warn('Input[name="product_title"] not found or title not detected');
+
+    } catch (error) {
+      console.error('Error loading reviews:', error);
+    }
+  });
+  
  $(document).ready(function () {
   $('#review-section #review-form').on('submit', function (e) {
     e.preventDefault();
